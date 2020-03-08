0dd42f97d206b3239c2690cde0070aa35fcf568a
Tbeaumont79 <tbeaumont79@icloud.com>
2020-03-08 22:29:12 +0100

    mysql image is now working need to focus on nginx

--- srcs/mysql/init_mysql.sh
+++ srcs/mysql/init_mysql.sh
@@ -1,47 +1,37 @@
 #!/bin/bash
 
-if [ -d /app/mysql ]; then
-	echo "MySql Dir is already created skiping database creation !"
+if [ ! -d /run/mysqld ]; then
+    mkdir -p /run/mysqld
+fi
+if [ -d /var/lib/mysql/mysql ]; then
+    echo "mysql dir is already present skipping the creation !"
 else
 	echo "MySql Dir is not found, Installing Database";
-	mysql_install_db --user=wordpressuser > /dev/null
+	mysql_install_db --user=mysql > /dev/null
 
-	if ["$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=root
-		echo "mySql root pass -- > $MYSQL_ROOT_PASSWORD"
-	fi
-	MYSQL_DATABASE=wordpress
-	MYSQL_USER=wordpressuser
-	MYSQL_PASSWORD=root
-	if [ ! -d "/run/mysqld" ]; then
-		mkdir -p /run/mysqld
-	fi
-
+	echo "mySql root pass -- > $MYSQL_ROOT_PASSWORD"
 	tmpfile=`mktmp`
 	if [ ! -f "$tmpfile" ]; then
 		return 1
 	fi
 
 	cat << EOF > $tmpfile
-USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("") WHERE user='root AND host='localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
 EOF
-
-if [ "$MYSQL_DATABASE" != "" ]; then
-	echo "creating database - > $MYSQL_DATABASE"
-	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tmpfile
-
-	if [ $MYSQL_USER != "" ]; then
-		echo "Creating user : $MYSQL_USER with password $MYSQL_PASSWORD"
-		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tmpfile
-	fi
+    echo "FLUSH PRIVILEGES;" >> $tmpfile
+    /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tmpfile
+    rm -f $tmpfile
 fi
 
-/usr/bin/mysqld --user=wordpressuser --bootstrap --verbose=0 < $tmpfile
-rm -f $tmpfile
-fi
-
-exec /user/bin/mysqld --user=wordpressuser --console
+echo "mysql is running in "
+echo "1"
+sleep 1
+echo "2"
+sleep 1
+echo "3"
+sleep 1
+echo "4"
+sleep 2
+echo "start runing !"
+exec /usr/bin/mysqld --user=mysql --console
